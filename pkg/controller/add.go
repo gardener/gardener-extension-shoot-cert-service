87d1e83d0a2f00f62d44e127e75b7e83c89cc683
Martin Weindel <martin.weindel@sap.com>
2024-12-11 10:49:59 +0100

    forward extension class to controller

--- pkg/controller/add.go
+++ pkg/controller/add.go
@@ -7,7 +7,12 @@ package controller
 import (
 	"context"
 
+	extensionscontroller "github.com/gardener/gardener/extensions/pkg/controller"
 	"github.com/gardener/gardener/extensions/pkg/controller/extension"
+	extensionspredicate "github.com/gardener/gardener/extensions/pkg/predicate"
+	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+	"github.com/gardener/gardener/pkg/extensions"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
@@ -36,6 +41,8 @@ type AddOptions struct {
 	ServiceConfig config.Configuration
 	// IgnoreOperationAnnotation specifies whether to ignore the operation annotation or not.
 	IgnoreOperationAnnotation bool
+	// ExtensionClass defines the extension class this extension is responsible for.
+	ExtensionClass extensionsv1alpha1.ExtensionClass
 }
 
 // AddToManager adds a controller with the default Options to the given Controller Manager.
@@ -46,13 +53,21 @@ func AddToManager(ctx context.Context, mgr manager.Manager) error {
 // AddToManagerWithOptions adds a controller with the given Options to the given manager.
 // The opts.Reconciler is being set with a newly instantiated actuator.
 func AddToManagerWithOptions(ctx context.Context, mgr manager.Manager, opts AddOptions) error {
+	predicates := extension.DefaultPredicates(ctx, mgr, DefaultAddOptions.IgnoreOperationAnnotation)
+	if opts.ExtensionClass == extensionsv1alpha1.ExtensionClassGarden {
+		predicates = extensionspredicate.DefaultControllerPredicates(DefaultAddOptions.IgnoreOperationAnnotation, extensionspredicate.IsInGardenNamespacePredicate)
+		extensionscontroller.GetCluster = func(context.Context, client.Reader, string) (*extensions.Cluster, error) {
+			return nil, nil
+		}
+	}
 	return extension.Add(mgr, extension.AddArgs{
-		Actuator:          NewActuator(mgr, opts.ServiceConfig),
+		Actuator:          NewActuator(mgr, opts.ServiceConfig, opts.ExtensionClass),
 		ControllerOptions: opts.ControllerOptions,
 		Name:              ControllerName,
 		FinalizerSuffix:   FinalizerSuffix,
 		Resync:            0,
-		Predicates:        extension.DefaultPredicates(ctx, mgr, DefaultAddOptions.IgnoreOperationAnnotation),
+		Predicates:        predicates,
 		Type:              Type,
+		ExtensionClass:    opts.ExtensionClass,
 	})
 }
