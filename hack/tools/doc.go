4dbb43a53399a9d020c4e78cc89052cebf87b3d2
Martin Weindel <martin.weindel@sap.com>
2026-07-08 16:09:52 +0200

    draft adjustments with PR https://github.com/gardener/gardener/pull/15218

---
+++ hack/tools/doc.go
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package tools
+
+import _ "github.com/gardener/gardener/hack/tools/tool"
